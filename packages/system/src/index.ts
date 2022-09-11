49dd6dd44d187d06feb63165aabdb9112ca40716
Praney Behl <praneybehl@gmail.com>
2022-09-11 22:24:40 +1000

    feat(system): add system exports

---
+++ packages/system/src/index.ts
@@ -0,0 +1,52 @@
+// core functions
+export { compose, createParser, createStyleFunction, get, system } from './core';
+
+// styled-system css
+export { css } from './css';
+
+// styled-system parsers
+export { animation } from './parsers/animation';
+export { background } from './parsers/background';
+export { border } from './parsers/border';
+export { color } from './parsers/color';
+export { extendedFlexbox } from './parsers/extendedFlexbox';
+export { extendedGrid } from './parsers/extendedGrid';
+export { flexbox } from './parsers/flexbox';
+export { grid } from './parsers/grid';
+export { layout } from './parsers/layout';
+export { other } from './parsers/other';
+export { position } from './parsers/position';
+export { shadow } from './parsers/shadow';
+export { space } from './parsers/space';
+export { transition } from './parsers/transition';
+export { typography } from './parsers/typography';
+
+// pseudo props and mixin
+export { pseudoMixin } from './pseudo';
+
+// styled-system shouldForwardProp
+export { allProps, createShouldForwardProp, sfp, shouldForwardProp } from './shouldForwardProp';
+
+// Export all types
+export type { CreateStyleFunctionArgs, Parser, Scale, StyleFn, SystemConfig } from './core';
+
+export type { AnimationProps } from './parsers/animation';
+export type { BackgroundProps } from './parsers/background';
+export type { BorderProps } from './parsers/border';
+export type { ColorProps } from './parsers/color';
+export type { ExtendedFlexboxProps } from './parsers/extendedFlexbox';
+export type { ExtendedGridProps } from './parsers/extendedGrid';
+export type { FlexboxProps } from './parsers/flexbox';
+export type { GridProps } from './parsers/grid';
+export type { LayoutProps } from './parsers/layout';
+export type { OtherProps } from './parsers/other';
+export type { PositionProps } from './parsers/position';
+export type { ShadowProps } from './parsers/shadow';
+export type { SpaceProps } from './parsers/space';
+export type { TransitionProps } from './parsers/transition';
+export type { TypographyProps } from './parsers/typography';
+
+export type { PseudoProps } from './pseudo';
+
+// shared types
+export * from './types';
