a18d7e3b0a9cd6c652a07e52d91e0095a95454a6
Praney Behl <praneybehl@gmail.com>
2022-09-11 22:11:19 +1000

    chore(system:core): add styleFunction, system and utils functions

---
+++ packages/system/src/core/styleFunction.ts
@@ -0,0 +1,54 @@
+import * as CSS from 'csstype';
+import { Scale } from './types';
+import { get } from './util';
+
+export interface StyleFn {
+  (value: any, scale: Scale | undefined, props: any): any;
+  properties?: string[];
+  property?: string;
+  scale?: string;
+  transform?: (scale: Scale | undefined, path: string | number, fallback?: any, props?: any) => any;
+  defaults?: Scale;
+}
+
+export interface CreateStyleFunctionArgs {
+  property?: keyof CSS.Properties;
+  properties?: Array<keyof CSS.Properties>;
+  scale?: string;
+  transform?: (scale: Scale | undefined, path: string | number, fallback?: any, props?: any) => any;
+  defaultScale?: Scale;
+}
+
+export function createStyleFunction({
+  properties,
+  property,
+  scale,
+  transform = get,
+  defaultScale,
+}: CreateStyleFunctionArgs): StyleFn {
+  const p = properties || (property ? [property] : []);
+
+  const styleFn: StyleFn = (_value, _scale, _props) => {
+    const result: { [key in keyof CSS.Properties]: any } = {};
+
+    const value = transform(_scale, _value, _value, _props);
+
+    if (value === null) {
+      return undefined;
+    }
+
+    p.forEach((prop) => {
+      result[prop] = value;
+    });
+
+    return result;
+  };
+
+  styleFn.properties = properties;
+  styleFn.property = property;
+  styleFn.scale = scale;
+  styleFn.transform = transform.name !== 'get' ? transform : undefined;
+  styleFn.defaults = defaultScale;
+
+  return styleFn;
+}
