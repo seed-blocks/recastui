2c613881ecbac62c8229abb9115761fbfdc41068
Praney Behl <praneybehl@gmail.com>
2022-09-11 22:20:09 +1000

    feat(system:shouldForwardProps): add function to forward system props

---
+++ packages/system/src/shouldForwardProp/index.ts
@@ -0,0 +1,64 @@
+import isPropValid from '@emotion/is-prop-valid';
+import memoize from '@emotion/memoize';
+import { compose } from '../core';
+import { animation } from '../parsers/animation';
+import { background } from '../parsers/background';
+import { border } from '../parsers/border';
+import { color } from '../parsers/color';
+import { extendedFlexbox } from '../parsers/extendedFlexbox';
+import { extendedGrid } from '../parsers/extendedGrid';
+import { flexbox } from '../parsers/flexbox';
+import { grid } from '../parsers/grid';
+import { layout } from '../parsers/layout';
+import { other } from '../parsers/other';
+import { position } from '../parsers/position';
+import { shadow } from '../parsers/shadow';
+import { space } from '../parsers/space';
+import { transition } from '../parsers/transition';
+import { typography } from '../parsers/typography';
+import { pseudoSelectors } from '../pseudo';
+
+const all = compose(
+  animation,
+  background,
+  border,
+  color,
+  extendedFlexbox,
+  extendedGrid,
+  flexbox,
+  grid,
+  layout,
+  other,
+  position,
+  shadow,
+  space,
+  transition,
+  typography,
+);
+
+/**
+ * All `system` props.
+ */
+export const allProps = [...all.propNames, ...Object.keys(pseudoSelectors)];
+
+/**
+ * Create a custom `shouldForwardProp` function.
+ * Use this when you don't want `system` props included.
+ */
+export function createShouldForwardProp(props: string[]) {
+  const regex = new RegExp(`^(${props.join('|')})$`);
+  return memoize((prop: string) => isPropValid(prop) && !regex.test(prop));
+}
+
+/**
+ * `shouldForwardProp` with all `system` props.
+ */
+export const shouldForwardProp = createShouldForwardProp(allProps);
+
+/**
+ * Creates a `shouldForwardProp` with all `system` props.
+ * Also allows you to add additional props to the `sfp` fn.
+ */
+export function sfp(props: string[]) {
+  return createShouldForwardProp([...allProps, ...props]);
+}
