33436382a5d6e017013aa48fdcb8caa6bd84c8cf
Praney Behl <praneybehl@gmail.com>
2023-02-18 22:20:09 +1100

    fix(docs): remove prism from CodeBlock breaking the build

--- apps/docs/src/components/CodeBlock.tsx
+++ apps/docs/src/components/CodeBlock.tsx
@@ -1,14 +1,12 @@
 import { Fragment, useEffect, useState } from 'react';
-import Highlight, { defaultProps, Language } from 'prism-react-renderer';
 import copy from 'copy-to-clipboard';
 import { Button } from '@recastui/react';
 
 export type CodeBlockProps = {
 	code: string;
-	lang?: Language;
 };
 
-export function CodeBlock({ code, lang = 'tsx' }: CodeBlockProps) {
+export function CodeBlock({ code }: CodeBlockProps) {
 	const [copied, setCopied] = useState(false);
 	useEffect(() => {
 		if (copied && code) copy(code);
@@ -22,33 +20,16 @@ export function CodeBlock({ code, lang = 'tsx' }: CodeBlockProps) {
 		};
 	}, [copied]);
 	return (
-		<Highlight {...defaultProps} code={code.trimEnd()} language={lang} theme={undefined}>
-			{({ className, style, tokens, getTokenProps }) => (
-				<div className='prose prose-gray relative'>
-					<Button
-						size='xs'
-						onClick={() => setCopied(true)}
-						className='absolute top-2 right-3 uppercase'>
-						{copied ? 'Copied' : 'Copy'}
-					</Button>
-					<pre className={className} style={style}>
-						<code>
-							{tokens.map((line, lineIndex) => (
-								<Fragment key={lineIndex}>
-									{line
-										.filter(token => !token.empty)
-										.map((token, tokenIndex) => (
-											<span key={tokenIndex} {...getTokenProps({ token })} />
-										))}
-									{'\n'}
-								</Fragment>
-							))}
-						</code>
-					</pre>
-				</div>
-			)}
-		</Highlight>
+		<div className='prose prose-gray relative'>
+			<Button
+				size='xs'
+				onClick={() => setCopied(true)}
+				className='absolute top-2 right-3 uppercase'>
+				{copied ? 'Copied' : 'Copy'}
+			</Button>
+			<pre>
+				<code>{code.trimEnd()}</code>
+			</pre>
+		</div>
 	);
 }
-
-export default CodeBlock;
