f47ac17cd3a9c46d52e8dbd9bd0249083943b6ba
Praney Behl <praneybehl@gmail.com>
2023-02-18 19:25:28 +1100

    chore: fix formatting

--- packages/themes/src/types.ts
+++ packages/themes/src/types.ts
@@ -1,48 +1,48 @@
 export interface ThemeColors {
-  default: string;
-  tomato: string;
-  red: string;
-  crimson: string;
-  pink: string;
-  plum: string;
-  purple: string;
-  violet: string;
-  indigo: string;
-  blue: string;
-  cyan: string;
-  teal: string;
-  green: string;
-  grass: string;
-  orange: string;
-  brown: string;
-  sky: string;
-  mint: string;
-  lime: string;
-  yellow: string;
-  amber: string;
-  gold: string;
-  bronze: string;
+	default: string;
+	tomato: string;
+	red: string;
+	crimson: string;
+	pink: string;
+	plum: string;
+	purple: string;
+	violet: string;
+	indigo: string;
+	blue: string;
+	cyan: string;
+	teal: string;
+	green: string;
+	grass: string;
+	orange: string;
+	brown: string;
+	sky: string;
+	mint: string;
+	lime: string;
+	yellow: string;
+	amber: string;
+	gold: string;
+	bronze: string;
 }
 
 export interface ThemeSizes {
-  xs: string;
-  sm: string;
-  base: string;
-  lg: string;
-  xl: string;
-  '2xl': string;
-  '3xl': string;
-  '4xl': string;
-  '5xl': string;
-  '6xl': string;
-  '7xl': string;
-  '8xl': string;
-  '9xl': string;
+	xs: string;
+	sm: string;
+	base: string;
+	lg: string;
+	xl: string;
+	'2xl': string;
+	'3xl': string;
+	'4xl': string;
+	'5xl': string;
+	'6xl': string;
+	'7xl': string;
+	'8xl': string;
+	'9xl': string;
 }
 
 export interface ThemeStateColors {
-  error: string;
-  info: string;
-  success: string;
-  warning: string;
+	error: string;
+	info: string;
+	success: string;
+	warning: string;
 }
