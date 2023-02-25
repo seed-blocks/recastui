bf68ae0ac2389df918af56ad18e297a1647583d6
Praney Behl <praneybehl@gmail.com>
2023-02-25 23:35:40 +1100

    chore: add accordion docs snippets

--- apps/docs/src/components/configs/accordion.ts
+++ apps/docs/src/components/configs/accordion.ts
@@ -1,4 +1,12 @@
 export const accordionSnippets = {
 	import: `import { Accordion } from '@recastui/react';`,
-	usage: `<Accordion />`,
+	usage: `<Accordion items={[{
+			title: 'Customization',
+			content: 'Colors, fonts, shadows and many other parts are customizable to fit your design needs',
+		},
+		{
+			title: 'Flexibility',
+			content: 'Configure components appearance and behavior with vast amount of settings or overwrite any part of component styles',
+		}
+	]} />`,
 };
