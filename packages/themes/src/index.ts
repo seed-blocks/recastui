3c3701f9dd96e6fa1ba0bc8c8630da5705bdc866
Praney Behl <praneybehl@gmail.com>
2023-02-25 23:32:49 +1100

    feat(plop): add plop theme templates

--- packages/themes/src/index.ts
+++ packages/themes/src/index.ts
@@ -1,5 +1,7 @@
 import type { ThemeColors, ThemeSizes, ThemeStateColors } from './types';
 
 export * from './button';
+export * from './accordion';
+// ADD NEW COMPONENTS EXPORTS HERE
 
 export type { ThemeColors, ThemeSizes, ThemeStateColors };
