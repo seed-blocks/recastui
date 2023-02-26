720c41c197765a4e359ea186d2627d2cc3279192
Praney Behl <praneybehl@gmail.com>
2023-02-26 15:25:56 +1100

    docs(Accordion): add custom Icon recipe to Accordion component

--- apps/docs/src/components/configs/accordion.ts
+++ apps/docs/src/components/configs/accordion.ts
@@ -59,4 +59,14 @@ export const accordionSnippets = {
 		}
 	]} />
 	`,
+	icon: `<Accordion iconRotationClassName='-rotate-45' icon={IconPlus} items={[{
+			title: 'Customization',
+			content: 'Colors, fonts, shadows and many other parts are customizable to fit your design needs',
+		},
+		{
+			title: 'Flexibility',
+			content: 'Configure components appearance and behavior with vast amount of settings or overwrite any part of component styles',
+		}
+	]} />
+	`,
 };
