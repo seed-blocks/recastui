ae58fb9582abb9ebc882c3f0d80f88bcd8451555
Praney Behl <praneybehl@gmail.com>
2023-03-10 22:31:03 +1100

    fix(build): update tsup config to export cjs build

--- packages/themes/tsup.config.ts
+++ packages/themes/tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig, Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
-  treeshake: true,
-  splitting: false,
-  entry: ['src/**/*.ts'],
-  format: ['esm'],
-  dts: true,
-  minify: true,
-  clean: true,
-  ...options,
+	treeshake: true,
+	splitting: false,
+	entry: ['src/**/*.ts'],
+	format: ['esm', 'cjs'],
+	dts: true,
+	minify: true,
+	clean: true,
+	...options,
 }));
