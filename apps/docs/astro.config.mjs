252831bde674820fb192ea84c4354c07b60dc334
Praney Behl <praneybehl@gmail.com>
2023-03-11 15:49:07 +1100

    feat(docs): update docs site metadata, add webmanifest & robots config

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -16,10 +14,42 @@ import sitemap from '@astrojs/sitemap';
 import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
+import robotsTxt from 'astro-robots-txt';
+
+// https://astro.build/config
+import webmanifest from 'astro-webmanifest';
+
+// https://astro.build/config
 export default defineConfig({
 	site: 'https://recastui.com',
 	adapter: vercel({
 		analytics: true,
 	}),
-	integrations: [tailwind(), react(), mdx(), sitemap(), prefetch()],
+	integrations: [
+		tailwind(),
+		react(),
+		mdx(),
+		sitemap(),
+		prefetch(),
+		robotsTxt(),
+		webmanifest({
+			/**
+			 * required
+			 **/
+			name: 'Recastui',
+
+			/**
+			 * optional
+			 **/
+			icon: '/favicon.svg', // source for favicon & icons
+
+			short_name: 'App',
+			description:
+				'Recastui is a UI toolkit that enables the development of accessible web applications and design systems using React(soon Solidjs, Svelte and others). It includes a collection of themeable UI components and primitives that can serve as a solid foundation for implementing design systems.',
+			start_url: '/',
+			theme_color: '#ffffff',
+			background_color: '#ffffff',
+			display: 'standalone',
+		}),
+	],
 });
