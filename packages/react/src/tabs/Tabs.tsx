1b0d98e09380751d1c8d3f967dcbf9bed4063fc6
Praney Behl <praneybehl@gmail.com>
2023-03-22 22:18:44 +1100

    feat(Tabs): add Tabs component

---
+++ packages/react/src/tabs/Tabs.tsx
@@ -0,0 +1,53 @@
+import React, {
+	useState,
+	createContext,
+	useEffect,
+	ReactNode,
+	Dispatch,
+	SetStateAction,
+} from 'react';
+import { cl } from '../utils';
+
+export type TabsContextType = {
+	activeIndex: number;
+	orientation?: 'vertical' | 'horizontal';
+	variant?: 'underline' | 'outline' | 'pills';
+	rounded: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full';
+	setActiveIndex: Dispatch<SetStateAction<number>>;
+};
+
+export type TabsProps = {
+	children: ReactNode;
+	defaultIndex?: number;
+	orientation?: 'vertical' | 'horizontal';
+	variant?: 'underline' | 'outline' | 'pills';
+	rounded?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full';
+	onTabChange?: (index: number) => void;
+};
+
+export const TabsContext = createContext<TabsContextType | undefined>(undefined);
+
+export const Tabs: React.FC<TabsProps> = ({
+	children,
+	orientation = 'horizontal',
+	variant = 'underline',
+	rounded = 'none',
+	defaultIndex = 0,
+	onTabChange,
+}) => {
+	const [activeIndex, setActiveIndex] = useState(defaultIndex);
+
+	useEffect(() => {
+		if (onTabChange) {
+			onTabChange(activeIndex);
+		}
+	}, [activeIndex, onTabChange]);
+
+	return (
+		<TabsContext.Provider value={{ activeIndex, setActiveIndex, orientation, variant, rounded }}>
+			<div className={cl({ 'flex flex-row': orientation === 'vertical' })}>{children}</div>
+		</TabsContext.Provider>
+	);
+};
+
+Tabs.displayName = 'Tabs';
