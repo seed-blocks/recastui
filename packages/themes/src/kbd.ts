7bdd0300135260d4789d6003963267bfe83944ea
Praney Behl <praneybehl@gmail.com>
2023-03-25 18:31:02 +1100

    feat(themes): add Kbd thems

--- packages/themes/src/kbd.ts
+++ packages/themes/src/kbd.ts
@@ -1,10 +1,16 @@
-import { cva } from 'class-variance-authority';
+import { cva, VariantProps } from 'class-variance-authority';
 
-export const kbd = cva(
-	[''],
-	{
-		variants: {},
-	}
-);
+export const kbd = cva('bg-main-2 border border-b-2 border-main-7', {
+	variants: {
+		size: {
+			sm: ['text-xs', 'py-1', 'px-2'],
+			md: ['text-sm', 'py-2', 'px-3'],
+			lg: ['text-md', 'py-3', 'px-4'],
+		},
+	},
+	defaultVariants: {
+		size: 'sm',
+	},
+});
 
-export type KbdTheme = typeof kbd;
+export type KbdTheme = VariantProps<typeof kbd>;
