47a60ba48dc6590680e70e43f9952dc246c4c840
Praney Behl <praneybehl@gmail.com>
2023-03-25 18:38:46 +1100

    fix(Kbd): fix build error

--- packages/react/src/kbd/Kbd.tsx
+++ packages/react/src/kbd/Kbd.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { kbd, KbdTheme } from '@recastui/themes';
 import { cl } from '../utils';
 
-export interface KbdProps extends React.HTMLAttributes<'kbd'>, KbdTheme {
+export interface KbdProps extends React.HTMLAttributes<HTMLSpanElement>, KbdTheme {
 	className?: string;
 }
 
-export const Kbd = React.forwardRef<'kbd', KbdProps>(({ className, size, ...props }, ref) => {
-	const kbdClassName = cl(kbd({ size, className }));
-	return <kbd ref={ref} className={kbdClassName} {...props} />;
-});
+export const Kbd = React.forwardRef<HTMLSpanElement, KbdProps>(
+	({ className, size, ...props }, ref) => {
+		const kbdClassName = cl(kbd({ size, className }));
+		return <span ref={ref} className={kbdClassName} {...props} />;
+	},
+);
 
 Kbd.displayName = 'Kbd';
