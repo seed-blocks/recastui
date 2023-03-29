31dbc4d88455ce6337530e7c84f4dc8bb4bde9bb
Praney Behl <praneybehl@gmail.com>
2023-03-29 17:29:01 +1100

    feat(Alert): add Alert component

---
+++ packages/react/src/alert/Alert.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { alert, AlertTheme } from '@recastui/themes';
+import { cl, uuid } from '../utils';
+
+export type AlertProps = React.HTMLAttributes<HTMLDivElement> &
+	AlertTheme & {
+		icon: React.ReactNode;
+		title?: React.ReactNode;
+		description?: React.ReactNode;
+		className?: string;
+	};
+
+export const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
+	({ icon: AlertIcon, title, description, color, variant, className, children, ...props }, ref) => {
+		const titleId = `alert-title-${uuid()}`;
+		const bodyId = `alert-body-${uuid()}`;
+		const alertClasses = cl(alert.base({ color, variant, className }));
+		return (
+			<div
+				className={alertClasses}
+				ref={ref}
+				role='alert'
+				aria-labelledby={titleId}
+				aria-describedby={bodyId}
+				{...props}>
+				<AlertIcon className={cl(alert.icon())} />
+				<div className={cl(alert.body())}>
+					{title && (
+						<div id={titleId} className={cl(alert.title())}>
+							{title}
+						</div>
+					)}
+					{description && (
+						<div id={bodyId} className={cl(alert.description({ color }))}>
+							{description}
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	},
+);
+
+Alert.displayName = 'Alert';
