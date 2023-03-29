0c3112284ab909d58c62cd5ad82b92efb6678ca2
Praney Behl <praneybehl@gmail.com>
2023-03-29 18:12:36 +1100

    feat(Drawer): add Drawer component

---
+++ packages/react/src/drawer/Drawer.tsx
@@ -0,0 +1,87 @@
+import React, { createContext, useCallback, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { drawer, DrawerTheme } from '@recastui/themes';
+import { cl, uuid } from '../utils';
+
+export type DrawerContextProps = DrawerTheme & {
+	isOpen: boolean;
+	onClose: () => void;
+	closeOnOverlayClick?: boolean;
+	blockScrollOnMount?: boolean;
+	autoFocus?: boolean;
+	initialFocusRef?: React.RefObject<HTMLElement>;
+	finalFocusRef?: React.RefObject<HTMLElement>;
+	headerId?: string;
+	bodyId?: string;
+};
+
+export const DrawerContext = createContext<DrawerContextProps>({
+	isOpen: false,
+	onClose: () => {},
+});
+
+export type DrawerProps = DrawerContextProps & {
+	children: React.ReactNode;
+	className?: string;
+};
+
+export const Drawer: React.FC<DrawerProps> = ({
+	isOpen,
+	onClose,
+	closeOnOverlayClick = true,
+	blockScrollOnMount = true,
+	autoFocus = true,
+	placement,
+	initialFocusRef,
+	finalFocusRef,
+	size,
+	children,
+	className,
+}) => {
+	if (!isOpen) return null;
+
+	const handleOverlayClick = useCallback(
+		(e: React.MouseEvent<HTMLDivElement>) => {
+			if (closeOnOverlayClick && e.target === e.currentTarget) {
+				onClose();
+			}
+		},
+		[closeOnOverlayClick, onClose],
+	);
+
+	useEffect(() => {
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				onClose();
+			}
+		};
+		document.addEventListener('keydown', handleKeyDown);
+		return () => {
+			document.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [onClose]);
+
+	return createPortal(
+		<DrawerContext.Provider
+			value={{
+				isOpen,
+				onClose,
+				closeOnOverlayClick,
+				blockScrollOnMount,
+				autoFocus,
+				initialFocusRef,
+				finalFocusRef,
+				placement,
+				size,
+				headerId: `drawer-header-${uuid()}`,
+				bodyId: `drawer-body-${uuid()}`,
+			}}>
+			<div className={cl(drawer.overlay({ isOpen, className }))} onClick={handleOverlayClick}>
+				{children}
+			</div>
+		</DrawerContext.Provider>,
+		document.body,
+	);
+};
+
+Drawer.displayName = 'Drawer';
---
+++ packages/react/src/drawer/index.ts
@@ -0,0 +1,6 @@
+export * from './Drawer';
+export * from './DrawerBody';
+export * from './DrawerContent';
+export * from './DrawerHeader';
+export * from './DrawerFooter';
+export * from './DrawerCloseButton';
