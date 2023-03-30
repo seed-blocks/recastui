32416ebeb1f3adb7262da452ef2980ce7a33c311
Praney Behl <praneybehl@gmail.com>
2023-03-30 23:21:40 +1100

    feat(MenuItem): add MenuItem component

---
+++ packages/react/src/menu/MenuItem.tsx
@@ -0,0 +1,46 @@
+import React, { useRef, useEffect } from 'react';
+import { useMenuContext } from './MenuContext';
+
+interface MenuItemProps {
+	children: React.ReactNode;
+	index: number;
+	className?: string;
+}
+
+export const MenuItem: React.FC<MenuItemProps> = ({ children, index, className }) => {
+	const { menuId, focusedIndex, onItemClick, onItemKeyDown, registerItem, unregisterItem } =
+		useMenuContext();
+	const ref = useRef<HTMLLIElement>(null);
+
+	useEffect(() => {
+		registerItem(ref);
+		return () => unregisterItem(ref);
+	}, []);
+
+	useEffect(() => {
+		if (focusedIndex === index) {
+			ref.current?.focus();
+		}
+	}, [focusedIndex, index]);
+
+	const handleClick = () => {
+		onItemClick(index);
+	};
+
+	const handleKeyDown = (event: React.KeyboardEvent) => {
+		onItemKeyDown(event, index);
+	};
+
+	return (
+		<li
+			ref={ref}
+			id={`${menuId}-item-${index}`}
+			role='menuitem'
+			tabIndex={focusedIndex === index ? 0 : -1}
+			onClick={handleClick}
+			onKeyDown={handleKeyDown}
+			className={className}>
+			{children}
+		</li>
+	);
+};
