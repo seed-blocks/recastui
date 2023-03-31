9b1e1fefe54b0840d673bde11f0d196a43a68d5f
Praney Behl <praneybehl@gmail.com>
2023-03-31 23:52:31 +1100

    feat(themes): add Menu components theme

--- packages/themes/src/menu.ts
+++ packages/themes/src/menu.ts
@@ -5,7 +5,7 @@ export const menuList = cva('bg-main-3 shadow-md min-w-[200px]');
 export type MenuListTheme = VariantProps<typeof menuList>;
 
 export const menuItem = cva(
-	'flex items-center px-4 py-2 cursor-pointer hover:bg-main-5 focus:outline-none focus:ring-2 focus:ring-main-10',
+	'flex items-center px-4 py-2 cursor-pointer hover:bg-main-5 focus:outline-none focus:bg-main-5',
 );
 
 export type MenuItemTheme = VariantProps<typeof menuItem>;
