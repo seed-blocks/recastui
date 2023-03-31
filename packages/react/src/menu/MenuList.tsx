5c3977362eec6152f836451749cf530258faaa99
Praney Behl <praneybehl@gmail.com>
2023-03-31 23:53:37 +1100

    feat(MenuList): add MenuList component

--- packages/react/src/menu/MenuList.tsx
+++ packages/react/src/menu/MenuList.tsx
@@ -1,16 +1,18 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, ReactElement } from 'react';
 import { usePopper } from 'react-popper';
 import { useMenuContext } from './MenuContext';
 import { menuList } from '@recastui/themes';
+import { AnimatePresence, motion } from 'framer-motion';
 import { cl } from '../utils';
+import { MenuItemProps } from './MenuItem';
 
 interface MenuListProps {
 	children: React.ReactNode;
 	className?: string;
 }
 
-export const MenuList: React.FC<MenuListProps> = ({ children, className }) => {
-	const { isOpen, onClose, menuId } = useMenuContext();
+export const MenuList: React.FC<MenuListProps> = ({ children, className, ...rest }) => {
+	const { isOpen, onMenuClose, menuId } = useMenuContext();
 	const buttonRef = useRef<HTMLButtonElement>(null);
 	const listRef = useRef<HTMLUListElement>(null);
 	const { styles, attributes } = usePopper(buttonRef.current, listRef.current, {
@@ -21,13 +23,8 @@ export const MenuList: React.FC<MenuListProps> = ({ children, className }) => {
 		if (!isOpen) return;
 
 		const handleClickOutside = (event: MouseEvent) => {
-			if (
-				listRef.current &&
-				!listRef.current.contains(event.target as Node) &&
-				buttonRef.current &&
-				!buttonRef.current.contains(event.target as Node)
-			) {
-				onClose();
+			if (!listRef.current?.contains(event.target as Node)) {
+				onMenuClose();
 			}
 		};
 
@@ -37,20 +34,37 @@ export const MenuList: React.FC<MenuListProps> = ({ children, className }) => {
 		};
 	}, [isOpen]);
 
-	if (!isOpen) {
-		return null;
-	}
+	const childrenWithIndex = React.Children.map(children, (child, _index) => {
+		if (React.isValidElement(child)) {
+			return React.cloneElement(child as ReactElement<MenuItemProps>, {
+				_index,
+			});
+		}
+		return child;
+	});
 
 	return (
-		<ul
-			ref={listRef}
-			id={`${menuId}-list`}
-			role='menu'
-			tabIndex={-1}
-			className={cl(menuList({ className }))}
-			style={styles.popper}
-			{...attributes.popper}>
-			{children}
-		</ul>
+		<AnimatePresence>
+			{isOpen && (
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ duration: 0.3 }}
+					className='relative'
+					{...rest}>
+					<ul
+						ref={listRef}
+						id={`${menuId}-list`}
+						role='menu'
+						tabIndex={-1}
+						className={cl(menuList({ className }))}
+						style={styles.popper}
+						{...attributes.popper}>
+						{childrenWithIndex}
+					</ul>
+				</motion.div>
+			)}
+		</AnimatePresence>
 	);
 };
