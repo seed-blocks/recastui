6ece013f9aff9a2c8c533d7270a14b169422b8be
Praney Behl <praneybehl@gmail.com>
2023-04-08 17:41:34 +1000

    feat(Modal): refactor Modal components to use floating-ui instead

--- packages/react/src/modal/ModalBody.tsx
+++ packages/react/src/modal/ModalBody.tsx
@@ -1,20 +1,30 @@
-import React, { ReactNode, useContext } from 'react';
+import React, { forwardRef, useLayoutEffect, Ref, ReactNode } from 'react';
+import { useModalContext } from './ModalContext';
+import { useId } from '@floating-ui/react';
 import { modal } from '@recastui/themes';
 import { cl } from '../utils';
-import { ModalContext } from './Modal';
 
 export type ModalBodyProps = {
 	children: ReactNode;
 	className?: string;
 };
 
-export const ModalBody: React.FC<ModalBodyProps> = ({ children, className }) => {
-	const context = useContext(ModalContext);
-	return (
-		<div id={context.bodyId} className={cl(modal.body({ className }))}>
-			{children}
-		</div>
-	);
-};
+export const ModalBody = forwardRef(
+	({ className, children, ...props }: ModalBodyProps, ref: Ref<HTMLDivElement>) => {
+		const { setDescriptionId } = useModalContext();
+		const id = useId();
+
+		useLayoutEffect(() => {
+			setDescriptionId(id);
+			return () => setDescriptionId(undefined);
+		}, [id, setDescriptionId]);
+
+		return (
+			<div {...props} ref={ref} id={id} className={cl(modal.body({ className }))}>
+				{children}
+			</div>
+		);
+	},
+);
 
 ModalBody.displayName = 'ModalBody';
--- packages/react/src/modal/ModalContent.tsx
+++ packages/react/src/modal/ModalContent.tsx
@@ -1,57 +1,63 @@
-import React, { useCallback, useContext } from 'react';
+import React, { forwardRef, Ref, ReactNode } from 'react';
+import {
+	FloatingFocusManager,
+	FloatingOverlay,
+	FloatingPortal,
+	useMergeRefs,
+	useTransitionStyles,
+} from '@floating-ui/react';
+import { useModalContext } from './ModalContext';
 import { modal } from '@recastui/themes';
-import FocusLock from 'react-focus-lock';
-import { ModalContext } from './Modal';
 import { cl } from '../utils';
 
 export type ModalContentProps = {
 	className?: string;
-	children: React.ReactNode;
+	overlayClassName?: string;
+	children: ReactNode;
 };
 
-export const ModalContent = ({ className, children, ...props }: ModalContentProps) => {
-	const context = useContext(ModalContext);
+export const ModalContent = forwardRef(
+	({ className, overlayClassName, ...props }: ModalContentProps, propRef: Ref<HTMLDivElement>) => {
+		const {
+			context: floatingContext,
+			initialFocus,
+			returnFocus,
+			size,
+			isCentered,
+			...context
+		} = useModalContext();
+		const ref = useMergeRefs([context.refs.setFloating, propRef]);
 
-	const onActivation = useCallback(() => {
-		if (context.initialFocusRef?.current) {
-			context.initialFocusRef.current.focus();
-		}
-		if (context.blockScrollOnMount) {
-			document.body.style.overflow = 'hidden';
-		}
-	}, [context.initialFocusRef]);
+		const { isMounted, styles } = useTransitionStyles(floatingContext, {
+			duration: { open: 400 },
+		});
 
-	const onDeactivation = useCallback(() => {
-		if (context.finalFocusRef) {
-			context.finalFocusRef?.current?.focus();
-		}
-		if (context.blockScrollOnMount) {
-			document.body.style.overflow = '';
-		}
-	}, [context.finalFocusRef]);
-
-	const returnFocus = !context.finalFocusRef;
-
-	return (
-		<FocusLock
-			autoFocus={context.autoFocus}
-			onActivation={onActivation}
-			onDeactivation={onDeactivation}
-			returnFocus={returnFocus}
-			data-autofocus-inside={context.autoFocus}>
-			<div
-				role='dialog'
-				aria-modal='true'
-				aria-labelledby={context.headerId}
-				aria-describedby={context.bodyId}
-				className={cl(
-					modal.content({ size: context.size, isCentered: context.isCentered, className }),
+		return (
+			<FloatingPortal>
+				{isMounted && (
+					<FloatingOverlay
+						className={cl(modal.overlay({ isCentered, className: overlayClassName }))}
+						lockScroll>
+						<FloatingFocusManager
+							modal
+							context={floatingContext}
+							initialFocus={initialFocus}
+							returnFocus={returnFocus}>
+							<div
+								ref={ref}
+								aria-labelledby={context.labelId}
+								aria-describedby={context.descriptionId}
+								style={styles}
+								className={cl(modal.content({ size, isCentered, className }))}
+								{...context.getFloatingProps(props)}>
+								{props.children}
+							</div>
+						</FloatingFocusManager>
+					</FloatingOverlay>
 				)}
-				{...props}>
-				{children}
-			</div>
-		</FocusLock>
-	);
-};
+			</FloatingPortal>
+		);
+	},
+);
 
 ModalContent.displayName = 'ModalContent';
