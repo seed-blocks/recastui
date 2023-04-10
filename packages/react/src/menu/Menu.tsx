68f70d5d5493c7d84f23dc349c853578acdf7b09
Praney Behl <praneybehl@gmail.com>
2023-04-10 15:49:00 +1000

    feat(Menu): update Menu components to use floating-ui instead

--- packages/react/src/menu/Menu.tsx
+++ packages/react/src/menu/Menu.tsx
@@ -1,76 +1,22 @@
-import React, { useState, useRef, useMemo, useEffect } from 'react';
-import { uuid, executeIfFunction } from '../utils';
-import { MenuContext } from './MenuContext';
+import React, { forwardRef, HTMLProps } from 'react';
 
-interface MenuProps {
-	children: React.ReactNode;
-	className?: string;
-}
+import { FloatingTree, useFloatingParentNodeId } from '@floating-ui/react';
+import { MenuList, MenuListProps } from './MenuList';
 
-export const Menu: React.FC<MenuProps> = ({ children, className }) => {
-	const [isOpen, setIsOpen] = useState(false);
-	const menuId = uuid('menu');
-	const [focusedIndex, setFocusedIndex] = useState(-1);
-	const itemsRef = useRef<React.RefObject<HTMLElement>[]>([]);
+export const Menu = forwardRef<HTMLButtonElement, MenuListProps & HTMLProps<HTMLButtonElement>>(
+	(props, ref) => {
+		const parentId = useFloatingParentNodeId();
 
-	const onMenuOpen = () => {
-		setFocusedIndex(0);
-		setIsOpen(true);
-	};
-
-	const onMenuClose = () => {
-		setIsOpen(false);
-		setFocusedIndex(-1);
-	};
-
-	const onItemKeyDown = (event: React.KeyboardEvent, index: number, onClick?: () => void) => {
-		if (event.key === 'ArrowDown') {
-			event.preventDefault();
-			setFocusedIndex((index + 1) % itemsRef.current.length);
-		} else if (event.key === 'ArrowUp') {
-			event.preventDefault();
-			setFocusedIndex((index - 1 + itemsRef.current.length) % itemsRef.current.length);
-		} else if (event.key === 'Home') {
-			event.preventDefault();
-			setFocusedIndex(0);
-		} else if (event.key === 'End') {
-			event.preventDefault();
-			setFocusedIndex(itemsRef.current.length - 1);
-		} else if (event.key === 'Escape') {
-			onMenuClose();
-		} else if (event.key === 'Enter' || event.key === ' ') {
-			onClick?.();
-			onMenuClose();
+		if (parentId === null) {
+			return (
+				<FloatingTree>
+					<MenuList {...props} ref={ref} />
+				</FloatingTree>
+			);
 		}
-	};
 
-	const registerItem = (itemRef: React.RefObject<HTMLElement>) => {
-		itemsRef.current.push(itemRef);
-	};
+		return <MenuList {...props} ref={ref} />;
+	},
+);
 
-	const unregisterItem = (itemRef: React.RefObject<HTMLElement>) => {
-		itemsRef.current = itemsRef.current.filter(ref => ref !== itemRef);
-	};
-
-	const contextValue = useMemo(
-		() => ({
-			isOpen,
-			menuId,
-			focusedIndex,
-			onMenuClose,
-			onMenuOpen,
-			onItemKeyDown,
-			registerItem,
-			unregisterItem,
-		}),
-		[isOpen, menuId, focusedIndex],
-	);
-
-	return (
-		<MenuContext.Provider value={contextValue}>
-			<div data-menu className={className}>
-				{executeIfFunction(children, { isOpen, onMenuClose })}
-			</div>
-		</MenuContext.Provider>
-	);
-};
+Menu.displayName = 'Menu';
--- packages/react/src/menu/MenuList.tsx
+++ packages/react/src/menu/MenuList.tsx
@@ -1,70 +1,299 @@
-import React, { useRef, useEffect, ReactElement } from 'react';
-import { usePopper } from 'react-popper';
-import { useMenuContext } from './MenuContext';
-import { menuList } from '@recastui/themes';
-import { AnimatePresence, motion } from 'framer-motion';
-import { cl } from '../utils';
-import { MenuItemProps } from './MenuItem';
+import React, {
+	Children,
+	cloneElement,
+	ElementType,
+	forwardRef,
+	HTMLProps,
+	isValidElement,
+	ReactNode,
+	useEffect,
+	useRef,
+	useState,
+} from 'react';
 
-interface MenuListProps {
-	children: React.ReactNode;
-	className?: string;
+import {
+	autoUpdate,
+	flip,
+	FloatingFocusManager,
+	FloatingNode,
+	FloatingPortal,
+	offset,
+	safePolygon,
+	shift,
+	useClick,
+	useDismiss,
+	useFloating,
+	useFloatingNodeId,
+	useFloatingParentNodeId,
+	useFloatingTree,
+	useHover,
+	useInteractions,
+	useListNavigation,
+	useMergeRefs,
+	useRole,
+	useTypeahead,
+} from '@floating-ui/react';
+import { Button, ButtonProps } from '../button';
+import { menu } from '@recastui/themes';
+import { cl } from '../utils';
+
+const CaretRight = () => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		width='16'
+		height='16'
+		viewBox='0 0 24 24'
+		stroke-width='2'
+		stroke='currentColor'
+		fill='none'
+		stroke-linecap='round'
+		stroke-linejoin='round'>
+		<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+		<path d='M9 6l6 6-6 6'></path>
+	</svg>
+);
+
+export function extractTextFromChildren(children: ReactNode): string[] {
+	let strings: string[] = [];
+
+	React.Children.map(children, (child: ReactNode) => {
+		if (typeof child === 'string') {
+			strings.push(child);
+		} else if (React.isValidElement(child)) {
+			if (child.props.label) {
+				strings.push(extractTextFromChildren(child.props.label).join(''));
+			} else {
+				strings.push(extractTextFromChildren(child.props.children).join(''));
+			}
+		}
+	});
+
+	return strings;
 }
 
-export const MenuList: React.FC<MenuListProps> = ({ children, className, ...rest }) => {
-	const { isOpen, onMenuClose, menuId } = useMenuContext();
-	const buttonRef = useRef<HTMLButtonElement>(null);
-	const listRef = useRef<HTMLUListElement>(null);
-	const { styles, attributes } = usePopper(buttonRef.current, listRef.current, {
-		placement: 'bottom-start',
-	});
-
-	useEffect(() => {
-		if (!isOpen) return;
-
-		const handleClickOutside = (event: MouseEvent) => {
-			if (!listRef.current?.contains(event.target as Node)) {
-				onMenuClose();
-			}
-		};
-
-		document.addEventListener('mousedown', handleClickOutside);
-		return () => {
-			document.removeEventListener('mousedown', handleClickOutside);
-		};
-	}, [isOpen]);
-
-	const childrenWithIndex = React.Children.map(children, (child, _index) => {
-		if (React.isValidElement(child)) {
-			return React.cloneElement(child as ReactElement<MenuItemProps>, {
-				_index,
-			});
-		}
-		return child;
-	});
-
-	return (
-		<AnimatePresence>
-			{isOpen && (
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					exit={{ opacity: 0 }}
-					transition={{ duration: 0.3 }}
-					className='relative'
-					{...rest}>
-					<ul
-						ref={listRef}
-						id={`${menuId}-list`}
-						role='menu'
-						tabIndex={-1}
-						className={cl(menuList({ className }))}
-						style={styles.popper}
-						{...attributes.popper}>
-						{childrenWithIndex}
-					</ul>
-				</motion.div>
-			)}
-		</AnimatePresence>
-	);
+export type MenuListProps = {
+	initialOpen?: boolean;
+	label: ReactNode;
+	nested?: boolean;
+	children?: ReactNode;
+	rootMenuIcon?: ElementType;
+	nestedMenuIcon?: ElementType;
+	menuButtonProps?: ButtonProps;
+	className?: string;
 };
+
+export const MenuList = forwardRef<HTMLButtonElement, MenuListProps & HTMLProps<HTMLButtonElement>>(
+	(
+		{
+			children,
+			label,
+			initialOpen = false,
+			nested = false,
+			rootMenuIcon: RootIcon,
+			nestedMenuIcon: NestedIcon = CaretRight,
+			menuButtonProps: { className: rootButtonClassName, ...props } = {},
+			className,
+		},
+		forwardedRef,
+	) => {
+		const [isOpen, setIsOpen] = useState(initialOpen);
+		const [activeIndex, setActiveIndex] = useState<number | null>(null);
+		const [allowHover, setAllowHover] = useState(false);
+
+		const listItemsRef = useRef<Array<HTMLButtonElement | null>>([]);
+		const listContentRef = useRef(extractTextFromChildren(children));
+
+		const tree = useFloatingTree();
+		const nodeId = useFloatingNodeId();
+		const parentId = useFloatingParentNodeId();
+		const isNested = parentId != null;
+
+		const { x, y, strategy, refs, context } = useFloating<HTMLButtonElement>({
+			nodeId,
+			open: isOpen,
+			onOpenChange: setIsOpen,
+			placement: isNested ? 'right-start' : 'bottom-start',
+			middleware: [
+				offset({ mainAxis: isNested ? 0 : 4, alignmentAxis: isNested ? -4 : 0 }),
+				flip(),
+				shift(),
+			],
+			whileElementsMounted: autoUpdate,
+		});
+
+		const hover = useHover(context, {
+			enabled: isNested && allowHover,
+			delay: { open: 75 },
+			handleClose: safePolygon({
+				restMs: 25,
+				blockPointerEvents: true,
+			}),
+		});
+		const click = useClick(context, {
+			event: 'mousedown',
+			toggle: !isNested || !allowHover,
+			ignoreMouse: isNested,
+		});
+		const role = useRole(context, { role: 'menu' });
+		const dismiss = useDismiss(context);
+		const listNavigation = useListNavigation(context, {
+			listRef: listItemsRef,
+			activeIndex,
+			nested: isNested,
+			onNavigate: setActiveIndex,
+		});
+		const typeahead = useTypeahead(context, {
+			enabled: isOpen,
+			listRef: listContentRef,
+			onMatch: isOpen ? setActiveIndex : undefined,
+			activeIndex,
+		});
+
+		const { getReferenceProps, getFloatingProps, getItemProps } = useInteractions([
+			hover,
+			click,
+			role,
+			dismiss,
+			listNavigation,
+			typeahead,
+		]);
+
+		// Event emitter allows you to communicate across tree components.
+		// This effect closes all menus when an item gets clicked anywhere
+		// in the tree.
+		useEffect(() => {
+			if (!tree) return;
+
+			function handleTreeClick() {
+				setIsOpen(false);
+			}
+
+			function onSubMenuOpen(event: { nodeId: string; parentId: string }) {
+				if (event.nodeId !== nodeId && event.parentId === parentId) {
+					setIsOpen(false);
+				}
+			}
+
+			tree.events.on('click', handleTreeClick);
+			tree.events.on('menuopen', onSubMenuOpen);
+
+			return () => {
+				tree.events.off('click', handleTreeClick);
+				tree.events.off('menuopen', onSubMenuOpen);
+			};
+		}, [tree, nodeId, parentId]);
+
+		useEffect(() => {
+			if (isOpen && tree) {
+				tree.events.emit('menuopen', { parentId, nodeId });
+			}
+		}, [tree, isOpen, nodeId, parentId]);
+
+		// Determine if "hover" logic can run based on the modality of input. This
+		// prevents unwanted focus synchronization as menus open and close with
+		// keyboard navigation and the cursor is resting on the menu.
+		useEffect(() => {
+			function onPointerMove({ pointerType }: PointerEvent) {
+				if (pointerType !== 'touch') {
+					setAllowHover(true);
+				}
+			}
+
+			function onKeyDown() {
+				setAllowHover(false);
+			}
+
+			window.addEventListener('pointermove', onPointerMove, {
+				once: true,
+				capture: true,
+			});
+			window.addEventListener('keydown', onKeyDown, true);
+			return () => {
+				window.removeEventListener('pointermove', onPointerMove, {
+					capture: true,
+				});
+				window.removeEventListener('keydown', onKeyDown, true);
+			};
+		}, [allowHover]);
+
+		const referenceRef = useMergeRefs([refs.setReference, forwardedRef]);
+
+		return (
+			<FloatingNode id={nodeId}>
+				<Button
+					ref={referenceRef}
+					data-open={isOpen ? '' : undefined}
+					data-nested={isNested ? '' : undefined}
+					{...(isNested && { variant: 'unstyled' })}
+					{...getReferenceProps({
+						className: cl(menu.button({ isNested, rootButtonClassName })),
+						...props,
+						onClick(event) {
+							event.stopPropagation();
+						},
+						...(isNested && {
+							role: 'menuitem',
+						}),
+					})}>
+					{label}
+					{isNested
+						? NestedIcon && (
+								<span role='presentation' aria-hidden className='ml-2.5'>
+									<NestedIcon />
+								</span>
+						  )
+						: RootIcon && (
+								<span role='presentation' aria-hidden className='ml-2.5'>
+									<RootIcon />
+								</span>
+						  )}
+				</Button>
+				<FloatingPortal>
+					{isOpen && (
+						<FloatingFocusManager
+							context={context}
+							modal={false}
+							initialFocus={isNested ? -1 : 0}
+							returnFocus={!isNested}>
+							<div
+								ref={refs.setFloating}
+								className={cl(menu.list({ className }))}
+								style={{
+									position: strategy,
+									top: y ?? 0,
+									left: x ?? 0,
+									width: 'max-content',
+								}}
+								{...getFloatingProps()}>
+								{Children.map(
+									children,
+									(child, index) =>
+										isValidElement(child) &&
+										cloneElement(child, {
+											...getItemProps({
+												tabIndex: activeIndex === index ? 0 : -1,
+												ref(node: HTMLButtonElement) {
+													listItemsRef.current[index] = node;
+												},
+												onClick(event) {
+													child.props.onClick?.(event);
+													tree?.events.emit('click');
+												},
+												onMouseEnter() {
+													if (allowHover && isOpen) {
+														setActiveIndex(index);
+													}
+												},
+											}),
+										}),
+								)}
+							</div>
+						</FloatingFocusManager>
+					)}
+				</FloatingPortal>
+			</FloatingNode>
+		);
+	},
+);
+
+MenuList.displayName = 'MenuList';
