2d5c3c422bf1091bb6cb272c5da5bc1e75fa8b44
Praney Behl <2144887+praneybehl@users.noreply.github.com>
2023-09-16 09:33:57 +1000

    fix(Menu): styling issue preventing from adding max-height

--- packages/themes/src/menu.ts
+++ packages/themes/src/menu.ts
@@ -4,12 +4,12 @@ export const menu = {
 	button: cva('justify-between', {
 		variants: {
 			isNested: {
-				true: 'MenuItem focus-visible:outline-none hover:bg-main-7 focus-visible:bg-main-7 focus-visible:ring-0 focus-visible:border-0',
+				true: 'MenuItem focus-visible:outline-none hover:bg-main-7 focus-visible:bg-main-7 focus-visible:ring-0 focus-visible:border-0 w-full',
 				false: 'RootMenu',
 			},
 		},
 	}),
-	list: cva('outline-none bg-main-3 flex flex-col'),
+	list: cva('outline-none bg-main-3'),
 	item: cva(
 		'MenuItem flex items-center justify-start hover:bg-main-7 focus-visible:outline-none focus-visible:bg-main-7 focus-visible:ring-0 focus-visible:border-0',
 	),
